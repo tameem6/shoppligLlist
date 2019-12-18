1fe1728524ff3ee08f11b95e8ed8c19ee43fa93a
tameem6 <tameem6ansar@gmail.com>
2019-12-18 17:53:28 +0530

    Api for Items

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://tameem:91i2ta@cluster0-i1pux.mongodb.net/test?retryWrites=true&w=majority"
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/items');
+
+const app = express();
+
+//Body parser MW
+app.use(bodyParser.json());
+
+//DB Connect 
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db)
+    .then(() => console.log('Mongo Connected'))
+    .catch(err => console.log(err));
+
+//Routes
+app.use('/items', items);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
