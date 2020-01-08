68d12fa6e099ab0c5423a4ed49fca4e23ca1119f
tameem6 <tameem6ansar@gmail.com>
2020-01-08 17:32:57 +0530

    Finished clientside

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const items = require('./routes/items');
 
 const app = express();
@@ -19,6 +19,14 @@ mongoose.connect(db)
 //Routes
 app.use('/items', items);
 
+//Serve static assets if in productions
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
